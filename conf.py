0ba7f85c3f8953eaa7890e2637f0a8daddf61e07
Toshihiro Kamishima <mail@kamishima.net>
2012-04-22 13:55:22 +0900

    add outline.txt

--- conf.py
+++ conf.py
@@ -72,7 +72,7 @@ today_fmt = '%Y/%m/%d'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '00memo', 'readme.rst']
+exclude_patterns = ['_build', '00memo', 'readme.rst', 'outline.txt']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
