052e42b8d92d150337550ffe48b78270853712b5
Toshihiro Kamishima <mail@kamishima.net>
2012-04-25 04:00:46 +0900

    re-organize file structure

--- conf.py
+++ conf.py
@@ -72,7 +72,7 @@ today_fmt = '%Y/%m/%d'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '00memo', 'readme.rst', 'outline.txt']
+exclude_patterns = ['_build', '00memo', 'readme.rst', '_static/source/readme.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
