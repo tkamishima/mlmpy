2c3d7f8fc5eadef267937328096a4801af8a2607
Toshihiro Kamishima <mail@kamishima.net>
2012-04-25 13:21:36 +0900

    change code-block style

--- conf.py
+++ conf.py
@@ -94,7 +94,7 @@ exclude_patterns = ['_build', '00memo', 'readme.rst', '_static/source/readme.rst
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
